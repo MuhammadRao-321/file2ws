b2d74511bde45ab3ad31e7a5d9caeb88fc068723
MuhammadRao-321 <manurajpoot728@gmail.com>
2025-07-16 20:40:45 +0500

    Add files via upload

---
+++ Main2.py
@@ -0,0 +1,25 @@
+#function 1
+def fun1(n):
+ return n*(n+1)/2
+print(fun1(4))
+#time complexity = 1
+#more effecient
+
+def fun2(n):
+ sum = 0
+ for i in range(1, n+1):
+  sum += i
+ return sum
+print(fun2(4))
+ #time complexity = n
+ #average effecient
+
+def fun3(n):
+  sum = 0
+  for i in range(1, n+1):
+   for i in range(1, i+1):
+    sum += 1
+  return sum
+print(fun3(4))
+   #time complexity = n^2
+   #less effecient
